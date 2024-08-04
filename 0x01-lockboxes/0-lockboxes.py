c1bcec649b665c253d5825933534c32282c40bbe
Omar Jammeh <orms45@gmail.com>
2024-08-04 15:47:11 -0700

    correction pep8

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,7 +9,6 @@ def canUnlockAll(boxes):
     """method to determine if all boxes can open"""
     count = 0
     n = len(boxes)
-    
     for idx in range(1, n):
         for num, box in enumerate(boxes):
             if idx in box and idx != num:
